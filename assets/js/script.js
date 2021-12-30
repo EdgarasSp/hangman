26dcf794ba53ee7f6255bbe2c663ef5030d63ac9
EdgarasSp <dev.applications.esp@gmail.com>
2021-12-30 00:24:36 +0000

    Add function placeholder structure to .js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,51 @@
 console.log("connected...");
+
+// declare var
+
+
+// get elements
+
+
+// create html game-type cards
+
+
+// create html game-area based on game type
+
+function runGame (gameType){}
+
+function displayHtml (){}
+
+function randomWord (){}
+    function displayEasyWord () {}
+    function displayMediumWord () {}
+    function displayHardWord () {}
+
+function randomLetter (){}
+    function displayRandomLetter () {}
+
+function checkAnswer () {}
+
+function timerMedium (){}
+    function checkTimeOk () {}
+    function checkTimeLost () {}
+
+function timerHard () {}
+    function checkTimeOk () {}
+    function checkTimeLost () {}
+
+function livesTracker () {}
+    function checkGameWon () {}
+    function checkGameLost () {}
+
+function scoreTracker () {}
+    function incrementCorrectScore(){}
+    function incrementIncorrectScore(){}
+
+
+function actionTracker () {}
+
+function gameContinue () {}
+    function resetGame () {}
+
+
+function gameEnd () {}
